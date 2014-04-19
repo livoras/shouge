57de512246cfe436e84d88ee7dc36069211b3848
khhhshhh <iammfw@163.com>
2014-04-19 20:45:30 +0800

    add angular

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
+# -*- coding: utf-8 -*-
 import os
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='static/app/build/')
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  subtitle = u'你就是一个艺术家'
+  return render_template('index.html', subtitle=subtitle)
 
 if __name__ == '__main__':
   app.run(debug=True)
